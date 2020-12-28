566ab94954e792808f3edf72fe2f806a6231e35c
minecraft <minecraft>
2020-12-28 00:12:46 -0300

    Changing file structure

---
+++ update-and-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+echo "Locking files"
+git annex lock
+echo "Resetting local repo"
+git reset --hard
+echo "Pulling changes"
+git pull
+echo "Grabbing annex files from remote"
+git annex get
+echo "Unlocking files"
+git annex unlock
+
+docker-compose down -v
+docker-compose up --no-start
+docker-compose stop
