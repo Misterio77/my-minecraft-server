64e476932ddaf2bafcfff5b26614e6bf52bcbad5
Misterio77 <contato@misterio.me>
2020-12-27 21:30:25 -0300

    Changing permissions yet again

--- predeploy.sh
+++ predeploy.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
+echo "Locking files"
 git annex lock
+echo "Resetting local repo"
 git reset --hard
+echo "Pulling changes"
 git pull
 #git annex sync
+echo "Grabbing annex files from remote"
 git annex get
+echo "Unlocking files"
 git annex unlock
